2c7cc979143efe743c2a9d75351bc65dc250b59d
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-05-08 14:26:01 -0400

    Added exercise 3.3

---
+++ week-3/advanced-routing/desrouleaux-exercise-3.3.js
@@ -0,0 +1,47 @@
+/*
+============================================
+; Title:  desrouleaux-exercise-3.3.js
+; Author: Lance Desrouleaux
+; Date:  7 May 2019
+; Description: Advanced Routing
+;===========================================
+*/
+
+
+
+
+
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/:employeeId", function(request, response) {
+
+    var employeeId = parseInt(request.params.employeeId, 10);
+
+     response.render("index", {
+
+        employeeId: employeeId
+
+    })
+
+});
+
+
+ http.createServer(app).listen(8080, function() {
+
+   console.log("Application started on port: 8080");
+
+});
