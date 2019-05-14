35ca2454a4f3687257fd6cf16192069ecf59fc6b
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-05-14 15:57:28 -0400

    added assignment 4.4

---
+++ week-4/desrouleaux-assignment-4.4.js
@@ -0,0 +1,58 @@
+/*
+============================================
+; Title:  assignment 4.4
+; Author: lance Dersrouleaux
+; Date:   14 May 2019
+; Description: Demonstrates how to programmatically set
+;              Node.js status codes.
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get('/',function(req,res){
+  res.send('Well you just dont GET it... ')
+});
+
+app.post("/", function(req,res){
+  res.send("a POST request ");
+});
+
+app.put('/', function (req,res){
+  res.send('PUTS are so fun ');
+
+});
+
+app.delete('/',function(req,res){
+  res.send ("well you just made a DELETE ")
+})
+app.get("/not-found", function(request, response) {
+  response.status(404);
+  response.json({
+    error: "Resource not found."
+  });
+});
+
+app.get("/ok", function(request, response) {
+  response.status(200);
+  response.json({
+    message: "Page loaded correctly."
+  });
+});
+
+app.get("/not-implemented", function(request, response) {
+  response.status(501);
+  response.json({
+    error: "Page not implemented."
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
+
+
+
