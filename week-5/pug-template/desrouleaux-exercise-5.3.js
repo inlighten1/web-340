92220e1e3f7fb0e4d5deb8aa00bb524655e7cb22
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-05-21 10:41:23 -0400

    added exercise 5.3

--- week-5/pug-template/desrouleaux-exercise-5.3.js
+++ week-5/pug-template/desrouleaux-exercise-5.3.js
@@ -1,44 +1,32 @@
-
-
 const header = require('../desrouleaux-header-test.js')
 
-console.log(header.display("Lance", "Desrouleaux", "Excercise 5.2"));
+console.log(header.display("Lance", "Desrouleaux", "exercise 5.3"));
 console.log('');
 
 var express = require("express");
 
 var http = require("http");
 
+var pug = require("pug");
+
 var path = require("path");
 
-app = express();
+var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
 
-app.set("view engine", "ejs");
-
-var names = [
-
-  "Lance",
-
-  "John",
-
-  "Jacob",
-
-  "Harry"
-
-];
-
+app.set("view engine", "pug");
 
 app.get("/", function(request, response) {
 
-    response.render("views", {
+    response.render("index", {
 
-        names: names
+        message: 'This is a great Pug template example'
 
-    })
+    });
 
 });
+
 
 http.createServer(app).listen(8080, function() {
 
