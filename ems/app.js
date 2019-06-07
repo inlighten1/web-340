391e395eaf3ed0917e4f7d406464e2fd5dcca953
Lance Desrouleaux <ldesrouleaux@my365.bellevue.edu>
2019-06-07 18:55:10 -0400

    add exercise 8.3 updates

--- ems/app.js
+++ ems/app.js
@@ -14,10 +14,19 @@ var path = require("path");
 var logger = require("morgan");
 
 var helmet= require("helmet");
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 var app = express();
+
+var cookieParser = require("cookie-parser");
+
+var csrf = require("csurf");
+
+// setup csrf protection
+
+var csrfProtection = csrf({cookie: true});
+
 
 app.set("views", path.resolve(__dirname, "views"));
 
@@ -26,6 +35,35 @@ app.set("view engine", "ejs");
 app.use(helmet.xssFilter());
 
 app.use(logger("short"));
+
+
+
+
+//CSURF Required Dependencies
+
+app.use(bodyParser.urlencoded({
+
+  extended: true
+
+}));
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function(request, response, next) {
+
+  var token = request.csrfToken();
+
+  response.cookie('XSRF-TOKEN', token);
+
+  response.locals.csrfToken = token;
+
+  next();
+
+});
+
+
 
 
 
@@ -50,9 +88,48 @@ app.get("/new", function(request, response) {
 
   response.render("new", {
     title: "New employee page",
-    page: "new"
+    page: "new",
+    message: "New Employee Page"
   });
 });
+
+app.post("/process", function(req, res) {
+
+  if (!req.body.firstName) {
+    res.status(400).send('Entries must have a name');
+    return;
+  }
+
+  // get the request's form data
+  const firstName = req.body.firstName;
+  console.log(firstName);
+
+  const lastName = req.body.lastName;
+  console.log(lastName);
+
+  const title = req.body.title;
+  console.log(title);
+
+  // create a employee model
+  let employee = new Employee({
+    firstName: firstName,
+    lastName: lastName,
+    title: title
+
+  });
+
+  // save
+  employee.save(function(err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(employeeName + ' saved successfully!');
+      res.redirect('/');
+    }
+  });
+});
+
 
 //setup route to view page
 
